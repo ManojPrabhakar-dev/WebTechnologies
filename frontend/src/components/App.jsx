711b8d77e4060fe7fe82b9c5df6fc09ab6ee5027
ANALOG\MPrabhak <manojprbhkr363@gmail.com>
2021-09-06 10:18:07 +0530

    Fixed Header .footer Tag Issue

---
+++ frontend/src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Welcome to ProShop</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
