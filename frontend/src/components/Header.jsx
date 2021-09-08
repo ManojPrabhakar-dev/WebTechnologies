764716e8b6c12567745e824d10caf948b4674bd6
ANALOG\MPrabhak <manojprbhkr363@gmail.com>
2021-09-08 12:34:35 +0530

    Added Homescreen

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="#home">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <i class="fas fa-shopping-cart"></i> Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Header;
