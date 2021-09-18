d8d01a57b25853d47dd3347041a463d40fa76fd6
ANALOG\MPrabhak <manojprbhkr363@gmail.com>
2021-09-18 17:39:18 +0530

    userAuth - phase1

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,36 +1,15 @@
 import express from "express";
-import asyncHandler from "express-async-handler";
-import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getProductById,
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
-// @desc Fetch All Products
-// @route GET /api/products
-// @access public
-router.get(
-  "/",
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+//router.get("/", getProducts);
+router.route("/").get(getProducts);
 
-// @desc Fetch single product
-// @route GET /api/products/id
-// @access public
-router.get(
-  "/:id",
-  asyncHandler(async (req, res) => {
-    //const product = await Product.find( {_id = req.params.id});
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      //res.status(404).json({ message: "Product Not Found" });
-      res.status(404);
-      throw new Error("Product id Not Found ");
-    }
-  })
-);
+//router.get("/:id", getProductById);
+router.route("/:id").get(getProductById);
 
 export default router;
