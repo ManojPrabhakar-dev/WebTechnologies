cd947115fd1dd819de6d49ddbd3fce229f0feabd
ANALOG\MPrabhak <manojprbhkr363@gmail.com>
2021-09-18 22:11:44 +0530

    Authentication - phase1

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { authUser } from "../controllers/userController.js";
-
+import {
+  authUser,
+  registerUser,
+  getUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
+
+router.route("/").post(registerUser);
 
 //router.route("/login").post(authUser);
 router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
